955a7efdb44662a2d4707df4373f1c9c86f75472
joalon <joakimlonnegren@gmail.com>
2021-04-14 21:25:58 +0200

    Clean up

--- lib/Day1Lib.hs
+++ lib/Day1Lib.hs
@@ -8,16 +8,3 @@ parseToDirections input = map (\x -> case x of {
                             '(' -> Just Up;
                             ')' -> Just Down;
                             _ -> Nothing }) input
-
-
-
-  {-
-
-  input <- getInput 1
-  let intermediate = scanl (+) 0 parsed
-  let result1 = last intermediate
-  let result2 = length $ takeWhile (\x -> x /= -1) intermediate
-  putStrLn $ "Result of first part is: " ++ show result1
-  putStrLn $ "Result of second part is: " ++ show result2
-
-  -}
--- test/day1.hs
+++ test/day1.hs
@@ -6,22 +6,22 @@ import Day1Lib (ElevatorDirection(..), parseToDirections)
 main :: IO ()
 main = hspec $ do
 
-  describe "Parsing from string to the data type works" $ do
+  describe "Day 1: parsing from string to the data type works" $ do
     it "Turns a string into a list of ElevatorDirections" $
       parseToDirections "(())" `shouldBe` [Just Up, Just Up, Just Down, Just Down]
 
 
-  -- describe "(()) and ()() both result in floor 0" $ do 
+  -- describe "(()) and ()() both result in floor 0" $ do
 
-  
+
   -- describe "((( and (()(()( both result in floor 3" $ do
-  
-  
+
+
   -- describe "))((((( also results in floor 3" $ do
-  
-  
+
+
   -- describe "()) and ))( both result in floor -1 (the first basement level)" $ do
-  
-  
+
+
   -- describe "))) and )())()) both result in floor -3" $ do
 
